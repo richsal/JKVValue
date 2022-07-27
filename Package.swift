82974adce4619d9317bab95dc034aa3f90a6591a
Rich Salvatierra <rich@renewedvision.com>
2022-07-27 11:46:54 -0400

    update

--- Package.swift
+++ Package.swift
@@ -3,17 +3,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "JKVValue",
-    platforms: [.iOS(.v9), .macOS(.v10_10), .tvOS(.v9)],
-    products: [
-        .library(
-            name: "JKVValue",
-            targets: ["JKVValue"])
-    ],
-    targets: [
-        .target(
-            name: "JKVValue"
-            ]
-        )
-    ]
+	name: "JKVValue",
+	platforms: [
+		.macOS(.v10_10)
+	],
+	products: [
+		.library(
+			name: "JKVValue",
+			targets: [
+				"JKVValue"
+			]
+		)
+	],
+	targets: [
+		.target(
+			name: "JKVValue"
+		)
+	]
 )

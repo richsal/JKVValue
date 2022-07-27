ff89d8b60538b92a678e5a043f69a8311081de54
Rich Salvatierra <rich@renewedvision.com>
2022-07-27 11:35:17 -0400

    Package

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "JKVValue",
+    platforms: [.iOS(.v9), .macOS(.v10_10), .tvOS(.v9)],
+    products: [
+        .library(
+            name: "JKVValue",
+            targets: ["JKVValue"])
+    ],
+    targets: [
+        .target(
+            name: "JKVValue"
+            ]
+        )
+    ]
+)
